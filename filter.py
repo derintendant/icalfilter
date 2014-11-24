d29dcc87c7086e06f0c79ad63c7822fe91cbf50f
Dennis Stengele <d.stengele@derintendant.de>
2014-11-24 21:53:34 +0100

    Add ability to get iCalendar from URL

--- filter.py
+++ filter.py
@@ -1,16 +1,38 @@
-import os
-
 import sys
+from urllib.error import URLError
+import urllib.request
+import urllib.parse
+import cgi
+import posixpath
+
+from rfc3987 import match
 
 from icalendar import Calendar
 
 
-schedule = open(sys.argv[1])
-lookfor = [key.lower() for key in sys.argv[2:]]
+ical = sys.argv[1]
+filterList = sys.argv[2:]
+outputFilename = ''
 
-filtered = Calendar()
 
-calendar = Calendar.from_ical(schedule.read())
+def setOutputFilename():
+    global outputFilename, schedule
+    if match(ical, 'URI'): # Remote URL
+        try:
+            schedule = urllib.request.urlopen(ical, None)
+        except URLError:
+            print("Error while retrieving iCalendar file from URL: " + ical)
+            sys.exit(1)
+
+        cdHeader = schedule.getheader('Content-Disposition')
+        if not None == cdHeader:
+            _, params = cgi.parse_header(cdHeader)
+            outputFilename = params['filename']
+        else:
+            outputFilename = 'filtered.ics'
+    else: #Local file
+        schedule = open(ical)
+        outputFilename = posixpath.splitext(ical)[0] + '_filtered.ics'
 
 
 def printMessage():
@@ -25,18 +47,26 @@ def printMessage():
 
     print(' '.join(str(eventString) for eventString in msg))
 
+
 def writeFilteredFile():
-    global outputFilename, filtered_file
-    outputFilename = os.path.splitext(sys.argv[1])[0] + '_filtered.ics'
     filtered_file = open(outputFilename, 'wb')
     filtered_file.write(filtered.to_ical())
     filtered_file.close()
 
+
+setOutputFilename()
+
+filterList = [key.lower() for key in filterList]
+
+filtered = Calendar()
+
+calendar = Calendar.from_ical(schedule.read())
+
 for event in calendar.walk():
     if 'SUMMARY' in event.keys():
         summary = event['SUMMARY'].lower()
-        if any(key in summary for key in lookfor):
-            printMessage()                # Prints info message
-            filtered.add_component(event) # Adds component to resulting iCal file
+        if any(key in summary for key in filterList):
+            # printMessage()                # Prints info message
+            filtered.add_component(event)   # Adds component to resulting iCal file
 
 writeFilteredFile()
