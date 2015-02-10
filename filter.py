0587e3767f5ef1bea8df787575d7451f49a8d58d
Dennis Stengele <d.stengele@derintendant.de>
2015-02-10 17:45:49 +0100

    Update filter script to use stdin and an external filter file

--- filter.py
+++ filter.py
@@ -1,75 +1,82 @@
-import sys
-from urllib.error import URLError
-import urllib.request
-import urllib.parse
-import cgi
-import posixpath
+#!/usr/bin/env python3
 
-from rfc3987 import match
+import sys
+import posixpath
+import getopt
+
 from icalendar import Calendar
 
-if len(sys.argv) < 3:
-    print('ERROR: Missing iCal file or URL or filter criteria')
-    sys.exit(1)
 
-ical = sys.argv[1]
-filterList = sys.argv[2:]
-outputFilename = ''
+def usage():
+    print("Usage: " + sys.argv[0] + "[-i input_file] -f filter_file")
 
 
-def setOutputFilename():
-    global outputFilename, schedule
-    if match(ical, 'URI'): # Remote URL
+def main(argv):
+    inputFile = sys.stdin
+    fromStdin = True
+    outputFilename = 'ical_filtered.ics'
+    filterFilename=''
+    filterlist = list()
+    try:
+        opts, args = getopt.getopt(argv, "hi:f:", ["help", "input=", "filter="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        if opt in ("-i", "--input"):
+            inputFilename = arg                                               # TODO: exception check
+            outputFilename = posixpath.splitext(arg)[0] + '_filtered.ics'
+            fromStdin = False
+        if opt in ("-f", "--filter"):
+            filterFilename = arg
+
+    if filterFilename == '':
+        usage()
+        sys.exit(2)
+
+    if not fromStdin:
         try:
-            schedule = urllib.request.urlopen(ical, None)
-        except URLError:
-            print("ERROR: Error while retrieving iCalendar file from URL: " + ical)
+            inputFile = open(inputFilename)
+        except FileNotFoundError:
+            print("ERROR: Input File not found")
+            sys.exit(1)
+        except PermissionError:
+            print("ERROR: Permission denied while accessing Input File")
             sys.exit(1)
 
-        cdHeader = schedule.getheader('Content-Disposition')
-        if not None == cdHeader:
-            _, params = cgi.parse_header(cdHeader)
-            outputFilename = params['filename']
-            outputFilename = posixpath.splitext(outputFilename)[0] + '_filtered.ics'
-        else:
-            outputFilename = 'filtered.ics'
-    else: #Local file
-        schedule = open(ical)
-        outputFilename = posixpath.splitext(ical)[0] + '_filtered.ics'
+    try:
+        with open(filterFilename) as filterfile:
+            filterlist = filterfile.read().splitlines()
+    except FileNotFoundError:
+        print("ERROR: Filter File not found")
+        sys.exit(1)
+    except PermissionError:
+        print("ERROR: Permission denied while accessing Filter File")
+        sys.exit(1)
+
+    filterlist = [key.lower() for key in filterlist]
+
+    filtered = Calendar()
+
+    calendar = Calendar.from_ical(inputFile.read())
+
+    for event in calendar.walk():
+        if 'SUMMARY' in event.keys():
+            summary = event['SUMMARY'].lower()
+            if any(key in summary for key in filterlist):
+                filtered.add_component(event)   # Adds component to resulting iCal file
+
+    writeFile(filename=outputFilename, filecontents=filtered.to_ical())
 
 
-def printMessage():
-    msg = []
-    # Wenn recurring Event, Takt ausgeben, ansonsten Startdatum
-    if 'RRULE' in event and 'FREQ' in event['RRULE']:
-        msg.extend(event['RRULE']['FREQ'])
-    else:
-        msg.append(event['DTSTART'].dt)
-
-    msg.append(event['SUMMARY'])
-
-    print(' '.join(str(eventString) for eventString in msg))
-
-
-def writeFilteredFile():
-    filtered_file = open(outputFilename, 'wb')
-    filtered_file.write(filtered.to_ical())
+def writeFile(filename, filecontents):
+    filtered_file = open(filename, 'wb')
+    sys.stdout.write(filecontents.decode('utf-8'))
     filtered_file.close()
 
-
-setOutputFilename()
-
-filterList = [key.lower() for key in filterList]
-
-filtered = Calendar()
-
-calendar = Calendar.from_ical(schedule.read())
-
-for event in calendar.walk():
-    if 'SUMMARY' in event.keys():
-        summary = event['SUMMARY'].lower()
-        if any(key in summary for key in filterList):
-            # printMessage()                # Prints info message
-            filtered.add_component(event)   # Adds component to resulting iCal file
-
-writeFilteredFile()
+if __name__ == "__main__":
+    main(sys.argv[1:])
